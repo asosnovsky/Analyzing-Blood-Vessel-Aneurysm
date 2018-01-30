6aef3020b5cfb91fefeed6a7b7d39bbcabb6d094
Ariel Sosnovsky <arielsosnovksy@gmail.com>
2018-01-30 11:19:41 -0500

    using meanshift algo

--- step_02.py
+++ step_02.py
@@ -1,60 +1,53 @@
-
 # Library
-from base import S1_CENTERS_FILE, S2_SUB_GROUPS_FOLDER, delete_file_if_exists, create_clean_folder
-from numpy import array, ndarray
+from base import S1_CENTERS_FILE, S2_CLUSTERS_FILE, S2_CLUSTER_ALGO_FILE
 from pandas import read_csv, DataFrame
 from tqdm import tqdm
 
-# Progress
-pbar = tqdm(total=14)
+# ARBITRARY CONSTANTS 
+# need more work to figure optimal points, 
+# at the moment these are manually set
+MS_BANDWIDTH = 2.942783814571208
+MAX_DISTANCE_QUANTILE = 85
+
+# Start Loading Bar
+pbar = tqdm(total=5)
 
 # Read Data
-centers:DataFrame = read_csv(S1_CENTERS_FILE)
-
-pbar.update()
-# 5-Number Summary
-number_summary = centers.describe()
-
-split_point = array([
-    number_summary['centroid_x']['50%'], 
-    number_summary['centroid_y']['50%'], 
-    number_summary['centroid_z']['50%']
-])
-
-def split_by_point(dt:DataFrame, axis, num) -> (DataFrame, DataFrame):
-    return( 
-        dt[ dt[[axis]].as_matrix() > num ],
-        dt[ dt[[axis]].as_matrix() <= num ]
-    )
-
-pbar.update()
-# Split by X-axis
-grp_X_top, grp_X_bottom = split_by_point(centers, 'centroid_x', split_point[0])
+triangles:DataFrame = read_csv(S1_CENTERS_FILE)
 pbar.update()
 
-# Split by Y-axis
-grp_X_top_Y_top, grp_X_top_Y_bottom = split_by_point(grp_X_top, 'centroid_y', split_point[1])
-grp_X_bottom_Y_top, grp_X_bottom_Y_bottom = split_by_point(grp_X_bottom, 'centroid_y', split_point[1])
+# #############################################################################
+# Algo
+# #############################################################################
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from numpy import percentile, array
+from pandas import DataFrame, Series, merge
+
+# Get Just the centers
+triangle_centers = triangles[['centroid_x','centroid_y','centroid_z']].as_matrix()
 pbar.update()
 
-# Split by Z-axis
-grp_X_top_Y_top_Z_top, grp_X_top_Y_top_Z_bottom = split_by_point(grp_X_top_Y_top, 'centroid_z', split_point[2])
-grp_X_top_Y_bottom_Z_top, grp_X_top_Y_bottom_Z_bottom = split_by_point(grp_X_top_Y_bottom, 'centroid_z', split_point[2])
-
-grp_X_bottom_Y_top_Z_top, grp_X_bottom_Y_top_Z_bottom = split_by_point(grp_X_bottom_Y_top, 'centroid_z', split_point[2])
-grp_X_bottom_Y_bottom_Z_top, grp_X_bottom_Y_bottom_Z_bottom = split_by_point(grp_X_bottom_Y_bottom, 'centroid_z', split_point[2])
+# clustering with MeanShift
+ms = MeanShift(bandwidth=MS_BANDWIDTH, bin_seeding=True)
+ms.fit(triangle_centers)
 pbar.update()
+
+# Grab Cluster Labels
+triangles['ms_label'] = Series(ms.labels_, index=triangles.index)
+pbar.update()
+
+# Attach Cluster Centers
+centers_and_clusters = triangles.merge( 
+    DataFrame(ms.cluster_centers_, columns=['ms_x','ms_y','ms_z']), 
+    left_on='ms_label', 
+    right_index=True 
+)
 
 # Save
-create_clean_folder(S2_SUB_GROUPS_FOLDER);pbar.update()
-
-grp_X_top_Y_top_Z_top.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_top_Y_top_Z_top.csv', index=False);pbar.update()
-grp_X_top_Y_top_Z_bottom.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_top_Y_top_Z_bottom.csv', index=False);pbar.update()
-grp_X_top_Y_bottom_Z_top.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_top_Y_bottom_Z_top.csv', index=False);pbar.update()
-grp_X_top_Y_bottom_Z_bottom.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_top_Y_bottom_Z_bottom.csv', index=False);pbar.update()
-grp_X_bottom_Y_top_Z_top.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_bottom_Y_top_Z_top.csv', index=False);pbar.update()
-grp_X_bottom_Y_top_Z_bottom.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_bottom_Y_top_Z_bottom.csv', index=False);pbar.update()
-grp_X_bottom_Y_bottom_Z_top.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_bottom_Y_bottom_Z_top.csv', index=False);pbar.update()
-grp_X_bottom_Y_bottom_Z_bottom.to_csv(S2_SUB_GROUPS_FOLDER + 'grp_X_bottom_Y_bottom_Z_bottom.csv', index=False);pbar.update()
-
-
+merge(
+    DataFrame(ms.labels_, columns=['ms_label']),
+    DataFrame(ms.cluster_centers_, columns=['x','y','z']),
+    left_index=True, right_index=True
+).to_csv(S2_CLUSTER_ALGO_FILE, index=False)
+centers_and_clusters.to_csv(S2_CLUSTERS_FILE, index=False)
+pbar.update()
